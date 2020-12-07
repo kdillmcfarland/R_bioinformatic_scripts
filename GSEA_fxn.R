c5343f540f00257113e5fada9ad37de019ea7fa0
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2020-12-07 11:08:18 -0800

    Optimize plot size

--- GSEA_fxn.R
+++ GSEA_fxn.R
@@ -224,8 +224,8 @@ GSEA <- function(gene_list, gmt_file=NULL, gmt_ls=NULL, nperm=1000,
     plotname <- paste(plotdir, obj.name, ".pdf", sep="")
   }
   ggsave(plotname, plot, 
-         width = 8+length(unique(to.plot$group))*8, 
-         height = length(unique(to.plot$pathway)),
+         width = 3+length(unique(to.plot$group))*3, 
+         height = length(unique(to.plot$pathway))/2,
          limitsize = FALSE)
 } else{
   message("No significant terms plotted.")
